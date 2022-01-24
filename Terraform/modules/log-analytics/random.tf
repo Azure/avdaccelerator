dcbe9ad72f20b196e398cb50dede04867de0fb3e
Jen Sheerin <jensheerin@microsoft.com>
2022-01-23 20:02:37 -0500

    add terraform

---
+++ Terraform/modules/log-analytics/random.tf
@@ -0,0 +1,8 @@
+# generate a random string (consisting of four characters)
+# https://registry.terraform.io/providers/hashicorp/random/latest/docs/resources/string
+resource "random_string" "random" {
+  length  = 4
+  upper   = false
+  special = false
+}
+
