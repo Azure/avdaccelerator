33618a4574374350ecde694ffee7bb5bb35e550e
Jen Sheerin <jensheerin@microsoft.com>
2022-04-14 17:01:49 -0400

    update

--- Terraform/modules/avd/random.tf
+++
@@ -1,8 +0,0 @@
-# generate a random string (consisting of four characters)
-# https://registry.terraform.io/providers/hashicorp/random/latest/docs/resources/string
-resource "random_string" "random" {
-  length  = 4
-  upper   = false
-  special = false
-}
-
