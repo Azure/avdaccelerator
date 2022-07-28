67e31083dad7d3def45390f9d7ef852087d60bb8
Jen Sheerin <jensheerin@microsoft.com>
2022-07-28 11:37:12 -0400

    tfsec

--- workload/terraform/avdbaseline/keyvault.tf
+++ workload/terraform/avdbaseline/keyvault.tf
@@ -12,10 +12,14 @@ resource "azurerm_key_vault" "keyvault" {
   enabled_for_disk_encryption = true
   tenant_id                   = data.azurerm_client_config.current.tenant_id
   soft_delete_retention_days  = 7
-  purge_protection_enabled    = false
+  purge_protection_enabled    = true
 
   sku_name = "standard"
 
+  network_acls {
+    bypass         = "AzureServices"
+    default_action = "Deny"
+  }
   access_policy {
     tenant_id = data.azurerm_client_config.current.tenant_id
     object_id = data.azurerm_client_config.current.object_id
