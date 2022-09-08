3a18fb98660f74f8afc6f1ecd4051b151de9e2e4
Jen Sheerin <jensheerin@microsoft.com>
2022-09-08 13:57:27 -0400

    remove rg ref

--- workload/terraform/avdbaseline/appsecgrp.tf
+++ workload/terraform/avdbaseline/appsecgrp.tf
@@ -1,10 +1,5 @@
 data "azurerm_client_config" "cfg" {}
 
-resource "azurerm_resource_group" "rg" {
-  name     = var.rg_so
-  location = var.avdLocation
-}
-
 resource "azurerm_application_security_group" "example" {
   name                = "avdasg-${var.avdLocation}-${var.prefix}"
   location            = azurerm_resource_group.rg.location
