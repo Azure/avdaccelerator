8ea2ab7ca658968d63d1831996f45e21467de12a
Mohamed Faizal <mofaizal@microsoft.com>
2022-09-17 11:14:19 +0800

    resolve

--- workload/terraform/greenfield/avdbaseline/host.tf
+++ workload/terraform/greenfield/avdbaseline/host.tf
@@ -46,13 +46,7 @@ resource "azurerm_windows_virtual_machine" "avd_vm" {
   admin_username             = var.local_admin_username
   admin_password             = var.local_admin_password
   encryption_at_host_enabled = true
-<<<<<<< HEAD
    os_disk {
-=======
-  # To enable encryption at host you must ensure the feature is already enabled on the subscription with az feature register --name EncryptionAtHost  --namespace Microsoft.Compute. To check az feature show --name EncryptionAtHost --namespace Microsoft.Compute
-
-  os_disk {
->>>>>>> e7c8fb8808a146ff58112c64832c6661cd530140
     name                 = "${lower(var.prefix)}-${count.index + 1}"
     caching              = "ReadWrite"
     storage_account_type = "Standard_LRS"
