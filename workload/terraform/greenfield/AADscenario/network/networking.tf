dc69d9165eba1b071b2c6f9a4d41ecccd927d91c
Jen Sheerin <jensheerin@microsoft.com>
2022-11-09 14:32:24 -0500

    terraform update

--- workload/terraform/greenfield/AADscenario/network/networking.tf
+++ workload/terraform/greenfield/AADscenario/network/networking.tf
@@ -9,13 +9,6 @@ provider "azurerm" {
   alias           = "spoke"
   subscription_id = var.spoke_subscription_id
 }
-
-data "azurerm_virtual_network" "remote" {
-  provider            = azurerm.hub
-  name                = var.ad_vnet
-  resource_group_name = var.ad_rg
-}
-
 data "azurerm_virtual_network" "vnet" {
   provider            = azurerm.spoke
   name                = azurerm_virtual_network.vnet.name
@@ -46,29 +39,4 @@ resource "azurerm_subnet" "subnet" {
 resource "azurerm_subnet_network_security_group_association" "nsg_assoc" {
   subnet_id                 = azurerm_subnet.subnet.id
   network_security_group_id = azurerm_network_security_group.res-0.id
-}
-
-# data "azurerm_virtual_network" "ad_vnet_data" {
-#   name                = var.ad_vnet
-#   resource_group_name = var.ad_rg
-# }
-
-resource "azurerm_virtual_network_peering" "peer1" {
-  name                         = "peer_${var.prefix}_avdspoke_ad"
-  resource_group_name          = azurerm_resource_group.net.name
-  virtual_network_name         = azurerm_virtual_network.vnet.name
-  remote_virtual_network_id    = data.azurerm_virtual_network.remote.id //data.azurerm_virtual_network.ad_vnet_data.id
-  allow_virtual_network_access = true
-  allow_forwarded_traffic      = true
-  provider                     = azurerm.spoke
-}
-
-resource "azurerm_virtual_network_peering" "peer2" {
-  name                         = "peer_${var.prefix}_ad_avdspoke"
-  resource_group_name          = var.ad_rg
-  virtual_network_name         = var.ad_vnet
-  remote_virtual_network_id    = data.azurerm_virtual_network.vnet.id //azurerm_virtual_network.vnet.id
-  allow_virtual_network_access = true
-  allow_forwarded_traffic      = true
-  provider                     = azurerm.hub
 }
