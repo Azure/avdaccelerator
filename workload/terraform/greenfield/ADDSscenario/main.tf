7482cd012862cba01e60e60129f0f4416413a343
Jen Sheerin <jensheerin@microsoft.com>
2022-12-01 17:40:49 -0500

    refactor

---
+++ workload/terraform/greenfield/ADDSscenario/main.tf
@@ -0,0 +1,64 @@
+# Creates Azure Virtual Desktop Insights Log Analytics Workspace
+module "avdi" {
+  source      = "../../modules/insights"
+  avdLocation = var.avdLocation
+  prefix      = var.prefix
+  rg_so       = "rg-avd-${substr(var.avdLocation, 0, 5)}-${var.prefix}-${var.rg_so}"
+
+}
+
+# Creates the Azure Virtual Desktop Spoke Network resources
+module "network" {
+  source                = "../../modules/network"
+  avdLocation           = var.avdLocation
+  rg_network            = "rg-avd-${substr(var.avdLocation, 0, 5)}-${var.prefix}-${var.rg_network}" //var.rg_network
+  vnet                  = "${var.vnet}-${substr(var.avdLocation, 0, 5)}-${var.prefix}"              //var.vnet
+  snet                  = "${var.snet}-${substr(var.avdLocation, 0, 5)}-${var.prefix}"              //var.snet
+  dns_servers           = var.dns_servers
+  vnet_range            = var.vnet_range
+  nsg                   = "${var.nsg}-${substr(var.avdLocation, 0, 5)}-${var.prefix}" //var.nsg
+  prefix                = var.prefix
+  rt                    = "${var.rt}-${substr(var.avdLocation, 0, 5)}-${var.prefix}" //var.rt
+  ad_rg                 = var.ad_rg
+  ad_vnet               = var.ad_vnet
+  subnet_range          = var.subnet_range
+  hub_subscription_id   = var.hub_subscription_id
+  spoke_subscription_id = var.spoke_subscription_id
+}
+
+# Optional - creates AVD hostpool, remote application group, and workspace for remote apps
+# Uncomment out if needed - this is a separate module from the desktop one above
+# Remove /* at beginning and */ at the end to uncomment out the entire module
+/*
+module "poolremoteapp" {
+  source         = "./poolremoteapp"
+  ragworkspace   = "${var.ragworkspace}-${substr(var.avdLocation,0,5)}-${var.prefix}-remote"  //var.ragworkspace
+  raghostpool    = "${var.raghostpool}-${substr(var.avdLocation,0,5)}-${var.prefix}-poolremoteapp" //var.raghostpool
+  rag            = "${var.rag}-${substr(var.avdLocation,0,5)}-${var.prefix}" //var.rag
+  rg_so          = var.rg_so
+  avdLocation    = var.avdLocation
+  rg_shared_name = var.rg_shared_name
+  prefix         = var.prefix
+  rfc3339        = var.rfc3339
+  depends_on     = [module.avdi.avdLocation]
+}
+*/
+
+# Optional - creates AVD personal hostpool, desktop application group, and workspace for personal desktops
+# Uncomment out if needed - this is a separate module from the desktop one above
+# Remove /* at beginning and */ at the end to uncomment out the entire module
+/*
+module "personal" {
+  source         = "./personal"
+  personalpool   = "${var.personalpool}-${substr(var.avdLocation,0,5)}-${var.prefix}-personal" //var.personalpool
+  pworkspace     = "${var.pworkspace}-${substr(var.avdLocation,0,5)}-${var.prefix}-personal" //var.pworkspace
+  rg_so          = var.rg_so
+  avdLocation    = var.avdLocation
+  rg_shared_name = var.rg_shared_name
+  prefix         = var.prefix
+  rfc3339        = var.rfc3339
+  pag            = "${var.pag}-${substr(var.avdLocation,0,5)}-${var.prefix}" //var.pag
+  depends_on     = [module.avdi.avdLocation]
+}
+*/
+
---
+++ workload/terraform/modules/network/networking.tf
@@ -0,0 +1,52 @@
+resource "azurerm_virtual_network" "vnet" {
+  name                = "${var.vnet}-${substr(var.avdLocation, 0, 5)}-${var.prefix}"
+  address_space       = var.vnet_range
+  location            = azurerm_resource_group.net.location
+  resource_group_name = "rg-avd-${substr(var.avdLocation, 0, 5)}-${var.prefix}-${var.rg_network}"
+  tags                = local.tags
+  lifecycle { ignore_changes = [tags] }
+
+  depends_on = [azurerm_resource_group.net]
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                                      = "${var.snet}-${substr(var.avdLocation, 0, 5)}-${var.prefix}"
+  resource_group_name                       = "rg-avd-${substr(var.avdLocation, 0, 5)}-${var.prefix}-${var.rg_network}"
+  virtual_network_name                      = azurerm_virtual_network.vnet.name
+  address_prefixes                          = var.subnet_range
+  private_endpoint_network_policies_enabled = true
+  depends_on                                = [azurerm_resource_group.net]
+}
+
+resource "azurerm_subnet" "pesubnet" {
+  name                                      = "${var.pesnet}-${substr(var.avdLocation, 0, 5)}-${var.prefix}"
+  resource_group_name                       = "rg-avd-${substr(var.avdLocation, 0, 5)}-${var.prefix}-${var.rg_network}"
+  virtual_network_name                      = azurerm_virtual_network.vnet.name
+  address_prefixes                          = var.pesubnet_range
+  private_endpoint_network_policies_enabled = true
+  depends_on                                = [azurerm_resource_group.net]
+}
+
+resource "azurerm_subnet_network_security_group_association" "nsg_assoc" {
+  subnet_id                 = azurerm_subnet.subnet.id
+  network_security_group_id = azurerm_network_security_group.res-0.id
+}
+
+resource "azurerm_virtual_network_peering" "peer1" {
+  name                         = "peer_${var.prefix}_avdspoke_ad"
+  resource_group_name          = azurerm_resource_group.net.name
+  virtual_network_name         = azurerm_virtual_network.vnet.name
+  remote_virtual_network_id    = data.azurerm_virtual_network.remote.id //data.azurerm_virtual_network.ad_vnet_data.id
+  allow_virtual_network_access = true
+  allow_forwarded_traffic      = true
+  provider                     = azurerm.spoke
+}
+resource "azurerm_virtual_network_peering" "peer2" {
+  name                         = "peer_${var.prefix}_ad_avdspoke"
+  resource_group_name          = var.ad_rg
+  virtual_network_name         = var.ad_vnet
+  remote_virtual_network_id    = data.azurerm_virtual_network.vnet.id //azurerm_virtual_network.vnet.id
+  allow_virtual_network_access = true
+  allow_forwarded_traffic      = true
+  provider                     = azurerm.hub
+}
