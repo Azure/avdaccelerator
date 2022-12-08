cfd19e5b57b3854e0454e879457b54ca7311eb94
Jen Sheerin <jensheerin@microsoft.com>
2022-12-08 16:09:09 -0500

    tag update

--- workload/terraform/greenfield/ADDSscenario/locals.tf
+++ workload/terraform/greenfield/ADDSscenario/locals.tf
@@ -3,8 +3,9 @@ locals {
   storage_name       = lower(replace("stavd${var.prefix}${random_string.random.id}", "-", ""))
   registration_token = azurerm_virtual_desktop_host_pool_registration_info.registrationinfo.token
   tags = {
-    environment = var.prefix
-    source      = "https://github.com/Azure/avdaccelerator/tree/main/workload/terraform/avdbaseline"
+    environment        = var.prefix
+    source             = "https://github.com/Azure/avdaccelerator/tree/main/workload/terraform/avdbaseline"
+    cm-resource-parent = azurerm_virtual_desktop_host_pool.hostpool.id
   }
 }
 
