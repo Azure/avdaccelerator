3cd46c20c66e122894ef6588a78e75b78b40b160
Jen Sheerin <jensheerin@microsoft.com>
2022-12-08 17:18:04 -0500

    tag update

--- workload/terraform/modules/insights/law.tf
+++ workload/terraform/modules/insights/law.tf
@@ -5,5 +5,5 @@ resource "azurerm_log_analytics_workspace" "lawksp" {
   resource_group_name = azurerm_resource_group.share.name
   sku                 = "PerGB2018"
   retention_in_days   = 30
-  tags = local.tags
+
 }
