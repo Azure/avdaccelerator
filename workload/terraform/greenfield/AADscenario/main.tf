2bd20e2fe0afd02ba2aaed0c0fe92c83a7277e85
Jen Sheerin <jensheerin@microsoft.com>
2022-12-16 12:00:03 -0500

    aad update

--- workload/terraform/greenfield/AADscenario/main.tf
+++ workload/terraform/greenfield/AADscenario/main.tf
@@ -1,24 +1,27 @@
 # Creates Azure Virtual Desktop Insights Log Analytics Workspace
 module "avdi" {
-  source      = "./insights"
+  source      = "../../modules/insights"
   avdLocation = var.avdLocation
   prefix      = var.prefix
-  rg_so       = "rg-avd-${substr(var.avdLocation, 0, 5)}-${var.prefix}-${var.rg_so}"
+  rg_avdi     = var.rg_avdi
 }
 
 # Creates the Azure Virtual Desktop Spoke Network resources
 module "network" {
-  source                = "./network"
+  source = "../../modules/network"
   avdLocation           = var.avdLocation
-  rg_network            = "rg-avd-${substr(var.avdLocation, 0, 5)}-${var.prefix}-${var.rg_network}"
-  vnet                  = "${var.vnet}-${substr(var.avdLocation, 0, 5)}-${var.prefix}"
-  snet                  = "${var.snet}-${substr(var.avdLocation, 0, 5)}-${var.prefix}"
-  dns_servers           = var.dns_servers
+  rg_network            = var.rg_network
+  vnet                  = var.vnet
+  snet                  = var.snet
+  pesnet                = var.pesnet
   vnet_range            = var.vnet_range
   nsg                   = "${var.nsg}-${substr(var.avdLocation, 0, 5)}-${var.prefix}"
   prefix                = var.prefix
   rt                    = "${var.rt}-${substr(var.avdLocation, 0, 5)}-${var.prefix}"
+  ad_rg                 = var.ad_rg
+  ad_vnet               = var.ad_vnet
   subnet_range          = var.subnet_range
+  pesubnet_range        = var.pesubnet_range
   hub_subscription_id   = var.hub_subscription_id
   spoke_subscription_id = var.spoke_subscription_id
 }
