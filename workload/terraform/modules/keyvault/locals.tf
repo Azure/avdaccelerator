9fab65c6b3ab5915dde1a6b600d81a84bef85bd7
Jen Sheerin <jensheerin@microsoft.com>
2023-01-25 11:14:07 -0500

    readme and samples file update

--- workload/terraform/modules/keyvault/locals.tf
+++ workload/terraform/modules/keyvault/locals.tf
@@ -1,4 +1,4 @@
 locals {
-  kv_name = lower("kv-avd-${var.prefix}-${random_string.random.id}")
-  allow_list_ip      = var.allow_list_ip
+  kv_name       = lower("kv-avd-${var.prefix}-${random_string.random.id}")
+  allow_list_ip = var.allow_list_ip
 }
