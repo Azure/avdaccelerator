a521bbe29669e9ed8bde193c6b3d3a3864498daa
Jen Sheerin <jensheerin@microsoft.com>
2023-02-22 13:06:18 -0500

    update to firewall and add id sub

--- workload/terraform/greenfield/AADscenario/main.tf
+++ workload/terraform/greenfield/AADscenario/main.tf
@@ -8,22 +8,27 @@ module "avdi" {
 
 # Creates the Azure Virtual Desktop Spoke Network resources
 module "network" {
-  source                = "../../modules/network"
-  avdLocation           = var.avdLocation
-  rg_network            = var.rg_network
-  vnet                  = var.vnet
-  snet                  = var.snet
-  pesnet                = var.pesnet
-  vnet_range            = var.vnet_range
-  nsg                   = "${var.nsg}-${substr(var.avdLocation, 0, 5)}-${var.prefix}"
-  prefix                = var.prefix
-  rt                    = "${var.rt}-${substr(var.avdLocation, 0, 5)}-${var.prefix}"
-  hub_connectivity_rg   = var.hub_connectivity_rg
-  hub_vnet              = var.hub_vnet
-  subnet_range          = var.subnet_range
-  pesubnet_range        = var.pesubnet_range
-  hub_subscription_id   = var.hub_subscription_id
-  spoke_subscription_id = var.spoke_subscription_id
+  source                   = "../../modules/network"
+  avdLocation              = var.avdLocation
+  rg_network               = var.rg_network
+  vnet                     = var.vnet
+  snet                     = var.snet
+  pesnet                   = var.pesnet
+  vnet_range               = var.vnet_range
+  nsg                      = "${var.nsg}-${substr(var.avdLocation, 0, 5)}-${var.prefix}"
+  prefix                   = var.prefix
+  rt                       = "${var.rt}-${substr(var.avdLocation, 0, 5)}-${var.prefix}"
+  hub_connectivity_rg      = var.hub_connectivity_rg
+  hub_vnet                 = var.hub_vnet
+  subnet_range             = var.subnet_range
+  pesubnet_range           = var.pesubnet_range
+  next_hop_ip              = var.next_hop_ip
+  fw_policy                = var.fw_policy
+  hub_subscription_id      = var.hub_subscription_id
+  spoke_subscription_id    = var.spoke_subscription_id
+  identity_subscription_id = var.identity_subscription_id
+  identity_rg              = var.identity_rg
+  identity_vnet            = var.identity_vnet
 }
 
 
