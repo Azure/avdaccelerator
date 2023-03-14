b31a723cbad10f9ce8c51335824d55f7e499e9f1
Jen Sheerin <jensheerin@microsoft.com>
2023-03-14 12:14:57 -0400

    naming update

--- workload/terraform/modules/insights/locals.tf
+++ workload/terraform/modules/insights/locals.tf
@@ -1,5 +1,6 @@
+locals {
   tags = {
     environment        = var.prefix
     source             = "https://github.com/Azure/avdaccelerator/tree/main/workload/terraform/avdbaseline"
-    cm-resource-parent = azurerm_virtual_desktop_host_pool.hostpool.id
   }
+}
