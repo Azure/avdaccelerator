a6d41faf550e5e75e4666db9650b190948cf4fc5
danycontre <dcontreras@microsoft.com>
2023-04-12 15:44:32 -0500

    updates

--- workload/scripts/DSCStorageScripts/Configuration.ps1
+++ workload/scripts/DSCStorageScripts/Configuration.ps1
@@ -1,99 +1,141 @@
 <#
     .SYNOPSIS
-        A DSC configuration file for clean up resources
+        A DSC configuration file for domain joining storage account
 
     .DESCRIPTION
-        This script will be run on the management VM.
+        This script will be run on a domain joined session host under domain admin credentials.
 #>
 
 param
 (    
     [Parameter(Mandatory = $true)]
     [ValidateNotNullOrEmpty()]
-    [string] $dscPath,  
+    [string] $StorageAccountName,
 
     [Parameter(Mandatory = $true)]
     [ValidateNotNullOrEmpty()]
-    [string] $subscriptionId,
+    [string] $StorageAccountRG,
 
     [Parameter(Mandatory = $true)]
     [ValidateNotNullOrEmpty()]
-    [string] $serviceObjectsRgName,
+    [string] $ShareName,
 
     [Parameter(Mandatory = $true)]
     [ValidateNotNullOrEmpty()]
-    [string] $computeObjectsRgName,
+    [string] $DomainName,
 
     [Parameter(Mandatory = $true)]
     [ValidateNotNullOrEmpty()]
-    [string] $storageObjectsRgName,
+    [string] $CustomOuPath,
 
     [Parameter(Mandatory = $true)]
     [ValidateNotNullOrEmpty()]
-    [string] $networkObjectsRgName,
+    [string] $IdentityServiceProvider,
 
     [Parameter(Mandatory = $true)]
     [ValidateNotNullOrEmpty()]
-    [string] $monitoringObjectsRgName,
+    [string] $AzureCloudEnvironment,
 
     [Parameter(Mandatory = $true)]
     [ValidateNotNullOrEmpty()]
-    [string] $azureCloudEnvironment,
+    [string] $SubscriptionId,
 
     [Parameter(Mandatory = $true)]
     [ValidateNotNullOrEmpty()]
-    [string] $managementVmName
+    [string] $ClientId,
+
+    [Parameter(Mandatory = $true)]
+    [ValidateNotNullOrEmpty()]
+    [string] $OUName,
+
+    [Parameter(Mandatory = $true)]
+    [ValidateNotNullOrEmpty()]
+    [string] $CreateNewOU,
+
+    [Parameter(Mandatory = $true)]
+    [ValidateNotNullOrEmpty()]
+    [string] $StoragePurpose,
+
+    [Parameter(Mandatory = $true)]
+    [ValidateNotNullOrEmpty()]
+    [string] $DomainAdminUserName,
+	
+    [Parameter(Mandatory = $true)]
+    [ValidateNotNullOrEmpty()]
+    [string] $DomainAdminUserPassword
 )
 
 
-Configuration temResourcesCleanUp
+Configuration DomainJoinFileShare
 {
     param
     (    
         [Parameter(Mandatory = $true)]
         [ValidateNotNullOrEmpty()]
-        [string] $dscPath,  
-    
+        [string] $StorageAccountName,
+
         [Parameter(Mandatory = $true)]
         [ValidateNotNullOrEmpty()]
-        [string] $subscriptionId,
-    
+        [string] $StorageAccountRG,
+
         [Parameter(Mandatory = $true)]
         [ValidateNotNullOrEmpty()]
-        [string] $serviceObjectsRgName,
-    
+        [string] $ShareName,
+
         [Parameter(Mandatory = $true)]
         [ValidateNotNullOrEmpty()]
-        [string] $computeObjectsRgName,
-    
+        [string] $DomainName,
+
         [Parameter(Mandatory = $true)]
         [ValidateNotNullOrEmpty()]
-        [string] $storageObjectsRgName,
-    
+        [string] $CustomOuPath,
+
         [Parameter(Mandatory = $true)]
         [ValidateNotNullOrEmpty()]
-        [string] $networkObjectsRgName,
-    
+        [string] $IdentityServiceProvider,
+
         [Parameter(Mandatory = $true)]
         [ValidateNotNullOrEmpty()]
-        [string] $monitoringObjectsRgName,
-    
+        [string] $AzureCloudEnvironment,
+
         [Parameter(Mandatory = $true)]
         [ValidateNotNullOrEmpty()]
-        [string] $azureCloudEnvironment,
-    
+        [string] $SubscriptionId,
+
         [Parameter(Mandatory = $true)]
         [ValidateNotNullOrEmpty()]
-        [string] $managementVmName
+        [string] $ClientId,
+
+        [Parameter(Mandatory = $true)]
+        [ValidateNotNullOrEmpty()]
+        [string] $OUName,
+
+        [Parameter(Mandatory = $true)]
+        [ValidateNotNullOrEmpty()]
+        [string] $CreateNewOU,
+
+        [Parameter(Mandatory = $true)]
+        [ValidateNotNullOrEmpty()]
+        [string] $StoragePurpose,
+
+        [Parameter(Mandatory = $true)]
+        [ValidateNotNullOrEmpty()]
+        [string] $DomainAdminUserName,
+	
+        [Parameter(Mandatory = $true)]
+        [ValidateNotNullOrEmpty()]
+        [string] $DomainAdminUserPassword
     )
     
     # Import the module that contains the File resource.
     Import-DscResource -ModuleName PsDesiredStateConfiguration
+    $secStringPassword = ConvertTo-SecureString $DomainAdminUserPassword -AsPlainText -Force
+    $DomainAdminCred = New-Object System.Management.Automation.PSCredential ($DomainAdminUserName, $secStringPassword)
 
     $ErrorActionPreference = 'Stop'
     
     $ScriptPath = [system.io.path]::GetDirectoryName($PSCommandPath)
-    . (Join-Path $ScriptPath "logger.ps1")
+    . (Join-Path $ScriptPath "Logger.ps1")
     
     Node localhost
     {
@@ -109,19 +151,49 @@ Configuration temResourcesCleanUp
                 return @{'Result' = '' }
             }
             SetScript            = {
-                . (Join-Path $using:ScriptPath "logger.ps1")
+                . (Join-Path $using:ScriptPath "Logger.ps1")
                 try {
-                    Write-Log "Cleaning up temporal deployment resources"
-                    & "$using:ScriptPath\postDeploymentTempResourcesCleanUp.ps1" -StorageAccountName $Using:StorageAccountName -StorageAccountRG $Using:StorageAccountRG -SubscriptionId $Using:SubscriptionId -ClientId $Using:ClientId -ShareName $Using:ShareName -DomainName $Using:DomainName -IdentityServiceProvider $Using:IdentityServiceProvider -AzureCloudEnvironment $Using:AzureCloudEnvironment -CustomOuPath $Using:CustomOuPath -OUName $Using:OUName -CreateNewOU $Using:CreateNewOU -StoragePurpose $Using:StoragePurpose
+                    Write-Log "DSC DomainJoinStorage SetScript Domain joining storage account $Using:StorageAccountName"
+                    & "$using:ScriptPath\Script-DomainJoinStorage.ps1" -StorageAccountName $Using:StorageAccountName -StorageAccountRG $Using:StorageAccountRG -SubscriptionId $Using:SubscriptionId -ClientId $Using:ClientId -ShareName $Using:ShareName -DomainName $Using:DomainName -IdentityServiceProvider $Using:IdentityServiceProvider -AzureCloudEnvironment $Using:AzureCloudEnvironment -CustomOuPath $Using:CustomOuPath -OUName $Using:OUName -CreateNewOU $Using:CreateNewOU -StoragePurpose $Using:StoragePurpose
 
-                    Write-Log "Successfully cleaned up of temporary resources"
+                    Write-Log "Successfully domain joined and/or NTFS permission set on Storage account"
                 }
                 catch {
                     $ErrMsg = $PSItem | Format-List -Force | Out-String
                     Write-Log -Err $ErrMsg
-                    throw [System.Exception]::new("Some error occurred in DSC postDeploymentTempResourcesCleanUp SetScript: $ErrMsg", $PSItem.Exception)
+                    throw [System.Exception]::new("Some error occurred in DSC DomainJoinStorage SetScript: $ErrMsg", $PSItem.Exception)
                 }
             }
+            TestScript           = {
+                . (Join-Path $using:ScriptPath "Logger.ps1")
+
+                try {
+                    Write-Log "DSC DomainJoinStorage TestScript checking if storage account $Using:StorageAccountName is domain joined."
+                    $ADModule = Get-Module -Name ActiveDirectory
+                    if (-not $ADModule) {
+                        return $False
+                    }
+                    else {
+                        Import-Module activedirectory
+                        $IsStorageAccountDomainJoined = Get-ADObject -Filter 'ObjectClass -eq "Computer"' | Where-Object { $_.Name -eq $Using:StorageAccountName }
+                        if ($IsStorageAccountDomainJoined) {
+                            Write-Log "Storage account $Using:StorageAccountName is already domain joined."
+                            return $True
+                        }
+                        else {
+                            Write-Log "Storage account $Using:StorageAccount is not domain joined."
+                            return $False
+                        }
+                    }
+                }
+                catch {
+                    $ErrMsg = $PSItem | Format-List -Force | Out-String
+                    Write-Log -Err $ErrMsg
+                    throw [System.Exception]::new("Some error occurred in DSC DomainJoinStorage TestScript: $ErrMsg", $PSItem.Exception)
+                }
+            }
+		
+            PsDscRunAsCredential = $DomainAdminCred
         }
     }
 }
