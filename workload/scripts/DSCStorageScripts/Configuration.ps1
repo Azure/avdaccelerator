23dd8bb5d0416fa28b98941d40df18f25c6b7425
danycontre <dcontreras@microsoft.com>
2023-04-12 13:24:50 -0500

    updates

--- workload/scripts/DSCStorageScripts/Configuration.ps1
+++ workload/scripts/DSCStorageScripts/Configuration.ps1
@@ -114,44 +114,14 @@ Configuration temResourcesCleanUp
                     Write-Log "Cleaning up temporal deployment resources"
                     & "$using:ScriptPath\postDeploymentTempResourcesCleanUp.ps1" -StorageAccountName $Using:StorageAccountName -StorageAccountRG $Using:StorageAccountRG -SubscriptionId $Using:SubscriptionId -ClientId $Using:ClientId -ShareName $Using:ShareName -DomainName $Using:DomainName -IdentityServiceProvider $Using:IdentityServiceProvider -AzureCloudEnvironment $Using:AzureCloudEnvironment -CustomOuPath $Using:CustomOuPath -OUName $Using:OUName -CreateNewOU $Using:CreateNewOU -StoragePurpose $Using:StoragePurpose
 
-                    Write-Log "Successfully domain joined and/or NTFS permission set on Storage account"
+                    Write-Log "Successfully cleaned up of temporary resources"
                 }
                 catch {
                     $ErrMsg = $PSItem | Format-List -Force | Out-String
                     Write-Log -Err $ErrMsg
-                    throw [System.Exception]::new("Some error occurred in DSC DomainJoinStorage SetScript: $ErrMsg", $PSItem.Exception)
+                    throw [System.Exception]::new("Some error occurred in DSC postDeploymentTempResourcesCleanUp SetScript: $ErrMsg", $PSItem.Exception)
                 }
             }
-            TestScript           = {
-                . (Join-Path $using:ScriptPath "Logger.ps1")
-
-                try {
-                    Write-Log "DSC DomainJoinStorage TestScript checking if storage account $Using:StorageAccountName is domain joined."
-                    $ADModule = Get-Module -Name ActiveDirectory
-                    if (-not $ADModule) {
-                        return $False
-                    }
-                    else {
-                        Import-Module activedirectory
-                        $IsStorageAccountDomainJoined = Get-ADObject -Filter 'ObjectClass -eq "Computer"' | Where-Object { $_.Name -eq $Using:StorageAccountName }
-                        if ($IsStorageAccountDomainJoined) {
-                            Write-Log "Storage account $Using:StorageAccountName is already domain joined."
-                            return $True
-                        }
-                        else {
-                            Write-Log "Storage account $Using:StorageAccount is not domain joined."
-                            return $False
-                        }
-                    }
-                }
-                catch {
-                    $ErrMsg = $PSItem | Format-List -Force | Out-String
-                    Write-Log -Err $ErrMsg
-                    throw [System.Exception]::new("Some error occurred in DSC DomainJoinStorage TestScript: $ErrMsg", $PSItem.Exception)
-                }
-            }
-		
-            PsDscRunAsCredential = $DomainAdminCred
         }
     }
 }
