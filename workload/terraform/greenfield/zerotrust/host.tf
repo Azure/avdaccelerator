28e841b51869d45bf8ac47cc4050064e59a20dbe
Jen Sheerin <jensheerin@microsoft.com>
2023-05-15 12:31:00 -0400

    add zt

--- workload/terraform/greenfield/zerotrust/host.tf
+++ workload/terraform/greenfield/zerotrust/host.tf
@@ -1,11 +1,4 @@
 
-# Creates Session Host 
-# data "azurerm_shared_image" "avd" {
-#   name                = var.image_name
-#   gallery_name        = var.gallery_name
-#   resource_group_name = var.image_rg
-# }
-
 resource "time_rotating" "avd_token" {
   rotation_days = 1
 }
@@ -18,12 +11,12 @@ resource "random_string" "AVD_local_password" {
   override_special = "*!@#?"
 }
 
+
 resource "azurerm_network_interface" "avd_vm_nic" {
-  count                         = var.rdsh_count
-  name                          = "${var.prefix}-${count.index + 1}-nic"
-  resource_group_name           = azurerm_resource_group.shrg.name
-  location                      = azurerm_resource_group.shrg.location
-  enable_accelerated_networking = true
+  count               = var.rdsh_count
+  name                = "${var.prefix}-${count.index + 1}-nic"
+  resource_group_name = azurerm_resource_group.shrg.name
+  location            = azurerm_resource_group.shrg.location
 
   ip_configuration {
     name                          = "nic${count.index + 1}_config"
@@ -32,19 +25,8 @@ resource "azurerm_network_interface" "avd_vm_nic" {
   }
 
   depends_on = [
-    azurerm_resource_group.shrg, module.network
+    azurerm_resource_group.shrg
   ]
-}
-
-# Availability Set
-resource "azurerm_availability_set" "aset" {
-  name                         = "avail-avd-${substr(var.avdLocation, 0, 5)}-${var.prefix}"
-  resource_group_name          = azurerm_resource_group.shrg.name
-  location                     = azurerm_resource_group.shrg.location
-  platform_fault_domain_count  = 2
-  platform_update_domain_count = 5
-  managed                      = true
-  tags                         = local.tags
 }
 
 resource "azurerm_windows_virtual_machine" "avd_vm" {
@@ -53,31 +35,19 @@ resource "azurerm_windows_virtual_machine" "avd_vm" {
   resource_group_name        = azurerm_resource_group.shrg.name
   location                   = azurerm_resource_group.shrg.location
   size                       = var.vm_size
-  license_type               = "Windows_Client"
   network_interface_ids      = ["${azurerm_network_interface.avd_vm_nic.*.id[count.index]}"]
   provision_vm_agent         = true
-  availability_set_id        = azurerm_availability_set.aset.id
   admin_username             = var.local_admin_username
   admin_password             = azurerm_key_vault_secret.localpassword.value
-  encryption_at_host_enabled = true //'Microsoft.Compute/EncryptionAtHost' feature is must be enabled in the subscription for this setting to work https://learn.microsoft.com/en-us/azure/virtual-machines/disks-enable-host-based-encryption-portal?tabs=azure-powershell
-  secure_boot_enabled        = true
-  vtpm_enabled               = true
-  tags                       = local.tags
+  //encryption_at_host_enabled = true //'Microsoft.Compute/EncryptionAtHost' feature is must be enabled in the subscription for this setting to work https://learn.microsoft.com/en-us/azure/virtual-machines/disks-enable-host-based-encryption-portal?tabs=azure-powershell
+
   os_disk {
     name                 = "${lower(var.prefix)}-${count.index + 1}"
     caching              = "ReadWrite"
     storage_account_type = "Standard_LRS"
+
   }
 
-  # To use marketplace image, uncomment the following lines and comment the source_image_id line
-  /*
-  source_image_reference {
-    publisher = var.vm_marketplace_mage.publisher
-    offer     = var.vm_marketplace_image.offer
-    sku       = var.vm_marketplace_image.sku
-    version   = var.vm_marketplace_image.version
-  }
-*/
 
   //source_image_id = data.azurerm_shared_image.avd.id
   source_image_id = "/subscriptions/${var.avdshared_subscription_id}/resourceGroups/${var.image_rg}/providers/Microsoft.Compute/galleries/${var.gallery_name}/images/${var.image_name}/versions/latest"
@@ -93,39 +63,26 @@ resource "azurerm_windows_virtual_machine" "avd_vm" {
   }
 }
 
-# Virtual Machine Extension for Domain Join
-resource "azurerm_virtual_machine_extension" "domain_join" {
+resource "azurerm_virtual_machine_extension" "aadjoin" {
   count                      = var.rdsh_count
-  name                       = "${var.prefix}-${count.index + 1}-domainJoin"
+  name                       = "${var.prefix}-${count.index + 1}-aadJoin"
   virtual_machine_id         = azurerm_windows_virtual_machine.avd_vm.*.id[count.index]
-  publisher                  = "Microsoft.Compute"
-  type                       = "JsonADDomainExtension"
-  type_handler_version       = "1.3"
+  publisher                  = "Microsoft.Azure.ActiveDirectory"
+  type                       = "AADLoginForWindows"
+  type_handler_version       = "1.0"
   auto_upgrade_minor_version = true
 
+  /*
+# Uncomment out settings for Intune
   settings = <<SETTINGS
-    {
-      "Name": "${var.domain_name}",
-      "OUPath": "${var.ou_path}",
-      "User": "${var.domain_user}@${var.domain_name}",
-      "Restart": "true",
-      "Options": "3"
-    }
+
+     {
+        "mdmId" : "0000000a-0000-0000-c000-000000000000"
+      }
 SETTINGS
-
-  protected_settings = <<PROTECTED_SETTINGS
-    {
-      "Password": "${var.domain_password}"
-    }
-PROTECTED_SETTINGS
-
-  lifecycle {
-    ignore_changes = [settings, protected_settings]
-  }
-
+*/
 }
 
-# Virtual Machine Extension for AVD Agent
 resource "azurerm_virtual_machine_extension" "vmext_dsc" {
   count                      = var.rdsh_count
   name                       = "${var.prefix}${count.index + 1}-avd_dsc"
@@ -154,12 +111,13 @@ SETTINGS
 PROTECTED_SETTINGS
 
   depends_on = [
-    azurerm_virtual_machine_extension.domain_join,
-    azurerm_virtual_desktop_host_pool.hostpool
+    azurerm_virtual_machine_extension.aadjoin,
+    azurerm_virtual_desktop_host_pool.hostpool,
+    data.azurerm_log_analytics_workspace.lawksp
   ]
 }
 
-# Virtual Machine Extension for MMA agent
+# MMA agent
 resource "azurerm_virtual_machine_extension" "mma" {
   name                       = "MicrosoftMonitoringAgent"
   count                      = var.rdsh_count
@@ -181,13 +139,13 @@ resource "azurerm_virtual_machine_extension" "mma" {
 PROTECTED_SETTINGS
 
   depends_on = [
-    azurerm_virtual_machine_extension.domain_join,
-    azurerm_virtual_machine_extension.vmext_dsc
+    azurerm_virtual_machine_extension.aadjoin,
+    azurerm_virtual_machine_extension.vmext_dsc,
+    data.azurerm_log_analytics_workspace.lawksp
   ]
-
 }
-
-# Virtual Machine Extension for Microsoft Antimalware
+/*
+# Microsoft Antimalware
 resource "azurerm_virtual_machine_extension" "mal" {
   name                       = "IaaSAntimalware"
   count                      = var.rdsh_count
@@ -198,8 +156,70 @@ resource "azurerm_virtual_machine_extension" "mal" {
   auto_upgrade_minor_version = "true"
 
   depends_on = [
-    azurerm_virtual_machine_extension.domain_join,
+    azurerm_virtual_machine_extension.aadjoin,
     azurerm_virtual_machine_extension.vmext_dsc,
     azurerm_virtual_machine_extension.mma
   ]
+}
+*/
+
+# Disk Encryption Set
+resource "azurerm_disk_encryption_set" "en-set" {
+  provider            = azurerm.spoke
+  name                = "des-${var.prefix}-01"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  key_vault_key_id    = azurerm_key_vault_key.stcmky.id
+
+  identity {
+    type = "SystemAssigned"
+  }
+  depends_on = [azurerm_key_vault.kv, azurerm_role_assignment.keystor, azurerm_key_vault_key.stcmky]
+
+}
+
+resource "azurerm_role_assignment" "ensetusr" {
+  scope                = azurerm_key_vault.kv.id
+  role_definition_name = "Key Vault Crypto Service Encryption User"
+  principal_id         = azurerm_disk_encryption_set.en-set.identity[0].principal_id
+   depends_on = [
+    time_sleep.wait
+  ]
+}
+
+# # Virtual Machine Extension for Disk Encryption set
+resource "azurerm_virtual_machine_extension" "cmkde" {
+  count                      = var.rdsh_count
+  name                       = "${var.prefix}-${count.index + 1}-cmkde"
+  virtual_machine_id         = azurerm_windows_virtual_machine.avd_vm.*.id[count.index]
+  publisher                  = "Microsoft.Azure.Security"
+  type                       = "AzureDiskEncryption"
+  type_handler_version       = "2.2"
+  auto_upgrade_minor_version = true
+/*
+  settings = <<SETTINGS
+  
+{
+  "EncryptionOperation": "EnableEncryption",
+  "KeyVaultURL": "https://${local.keyvault_name}.vault.azure.net/",
+  "KeyVaultResourceId": "/subscriptions/${var.spoke_subscription_id}/resourceGroups/${var.rg_so}/providers/Microsoft.KeyVault/vaults/${local.keyvault_name}",
+  "KeyEncryptionKeyURL": "https://${local.keyvault_name}.vault.azure.net/keys/azurerm_key_vault_key.stcmky.name/${azurerm_key_vault_key.stcmky.id}",
+  "KeyEncryptionKeyVaultResourceId": "/subscriptions/${var.spoke_subscription_id}/resourceGroups/${var.rg_so}/providers/Microsoft.KeyVault/vaults/${local.keyvault_name}",
+  "KeyEncryptionAlgorithm": "RSA-OAEP",
+  "VolumeType": "ALL"
+}
+SETTINGS
+}
+*/
+  settings = <<SETTINGS
+    {
+        "EncryptionOperation": "EnableEncryption",
+        "KeyVaultURL": "${azurerm_key_vault.kv.vault_uri}",
+        "KeyVaultResourceId": "${azurerm_key_vault.kv.id}",                  
+        "KeyEncryptionKeyURL": "${azurerm_key_vault_key.stcmky.id}",         
+        "KekVaultResourceId": "${azurerm_key_vault.kv.id}",  
+        "KeyEncryptionAlgorithm": "RSA-OAEP",
+        "VolumeType": "All"
+    }
+SETTINGS
 }
