ea6ddb173dc2a2b6e8f83dd73a182507b382416b
Jen Sheerin <jensheerin@microsoft.com>
2023-05-15 12:31:00 -0400

    add zt

---
+++ workload/terraform/modules/alerts/main.tf
@@ -0,0 +1,34 @@
+# Azure Action group for AVD Alerts
+resource "azurerm_monitor_action_group" "ag" {
+  provider            = azurerm.hub
+  name                = "AVD_Alert_ActionGroup"
+  resource_group_name = azurerm_resource_group.share.name
+  short_name          = "avdactiongroup"
+  email_receiver {
+    name          = "sendtoavdadmin"
+    email_address = "jensheerin@microsoft.com"
+  }
+}
+
+# Azure Alert for AVD VMs CPU
+resource "azurerm_monitor_metric_alert" "avd_alert_cpu" {
+  provider             = azurerm.hub
+  name                 = "AVD_Alert_CPU-metricalert"
+  resource_group_name  = azurerm_resource_group.share.name
+  target_resource_location = "Global"
+  scopes               = [azurerm_virtual_desktop_host_pool.hostpool.id]
+  description          = "AVD alert CPU"
+  target_resource_type = "Microsoft.Compute/virtualMachines"
+
+  criteria {
+    metric_namespace = "Microsoft.Compute/virtualMachines"
+    metric_name = "% Processor Time"  # "% Processor Time" is the metric name for CPU
+    aggregation = "Total"
+    operator  = "GreaterThan"
+    threshold = 10
+  }
+
+  action {
+    action_group_id = azurerm_monitor_action_group.ag.id
+  }
+}
