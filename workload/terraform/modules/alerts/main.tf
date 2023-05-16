4a35f388bbd079cb160a69416fb0fe99187b1f66
Jen Sheerin <jensheerin@microsoft.com>
2023-05-16 15:13:14 -0400

    alerts

--- workload/terraform/modules/alerts/main.tf
+++ workload/terraform/modules/alerts/main.tf
@@ -1,9 +1,15 @@
+# Create a Resource Group for Shared items like AVD Alerts
+resource "azurerm_resource_group" "rg_shared_name" {
+  name     = "rg-shared"
+  location = var.avdLocation
+}
+
 # Azure Action group for AVD Alerts
 resource "azurerm_monitor_action_group" "ag" {
   provider            = azurerm.hub
   name                = "AVD_Alert_ActionGroup"
-  resource_group_name = azurerm_resource_group.share.name
-  short_name          = "avdactiongroup"
+  resource_group_name = azurerm_resource_group.rg_shared_name.name
+  short_name          = "avdactgrp" #  short_name to be in the range (1 - 12)
   email_receiver {
     name          = "sendto'***REMOVED***'"
     email_address = "jensheerin@microsoft.com"
@@ -12,20 +18,21 @@ resource "azurerm_monitor_action_group" "ag" {
 
 # Azure Alert for AVD VMs CPU
 resource "azurerm_monitor_metric_alert" "avd_alert_cpu" {
-  provider             = azurerm.hub
-  name                 = "AVD_Alert_CPU-metricalert"
-  resource_group_name  = azurerm_resource_group.share.name
-  target_resource_location = "Global"
-  scopes               = [azurerm_virtual_desktop_host_pool.hostpool.id]
-  description          = "AVD alert CPU"
-  target_resource_type = "Microsoft.Compute/virtualMachines"
+  provider                 = azurerm.hub
+  name                     = "AVD_Alert_CPU-metricalert"
+  resource_group_name      = azurerm_resource_group.rg_shared_name.name
+  target_resource_location = var.avdLocation
+  scopes                   = ["/subscriptions/${var.hub_subscription_id}/resourceGroups/${var.rg_shared_name}"]
+  description              = "AVD alert CPU"
+  target_resource_type     = "Microsoft.Compute/virtualMachines"
+  severity = 3 # informational
 
   criteria {
     metric_namespace = "Microsoft.Compute/virtualMachines"
-    metric_name = "% Processor Time"  # "% Processor Time" is the metric name for CPU
-    aggregation = "Total"
-    operator  = "GreaterThan"
-    threshold = 10
+    metric_name      = "Percentage CPU" 
+    aggregation      = "Total"
+    operator         = "GreaterThan"
+    threshold        = 80
   }
 
   action {
