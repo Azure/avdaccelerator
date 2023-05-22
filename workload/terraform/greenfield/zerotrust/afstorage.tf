23220cd0610538e0aead0eeb0e704158cf415764
Jen Sheerin <jensheerin@microsoft.com>
2023-05-22 16:40:37 -0400

    zt update to encryption

--- workload/terraform/greenfield/zerotrust/afstorage.tf
+++ workload/terraform/greenfield/zerotrust/afstorage.tf
@@ -47,11 +47,6 @@ resource "azurerm_storage_account" "storage" {
     ]
   }
 
-  customer_managed_key {
-    key_vault_key_id          = azurerm_key_vault_key.stkek.id
-    user_assigned_identity_id = azurerm_user_assigned_identity.mi.id
-  }
-
   azure_files_authentication {
     directory_type = "AADKERB"
   }
