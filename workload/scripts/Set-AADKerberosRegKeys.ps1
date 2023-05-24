0e2933348c4d94effe9c91ed90f870fb503fa49c
Dany Contreras <78437433+danycontre@users.noreply.github.com>
2023-05-24 13:22:33 -0500

    Updates

---
+++ workload/scripts/Set-AADKerberosRegKeys.ps1
@@ -0,0 +1,9 @@
+﻿param(
+    [Parameter(Mandatory)]
+    [string]$storageAccountName,
+
+    [Parameter(Mandatory)]
+    [string]$identityDomainName
+)
+
+reg.exe add 'HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Lsa\Kerberos\domain_realm' /v $identityDomainName /d $storageAccountName.file.core.windows.net
