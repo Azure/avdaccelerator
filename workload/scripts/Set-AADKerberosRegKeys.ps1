b1a06a2bcb2cecca5dd017aef513396135a9fc8c
Dany Contreras <78437433+danycontre@users.noreply.github.com>
2023-05-24 13:28:03 -0500

    Updates

--- workload/scripts/Set-AADKerberosRegKeys.ps1
+++ workload/scripts/Set-AADKerberosRegKeys.ps1
@@ -7,3 +7,5 @@
 )
 
 reg.exe add 'HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Lsa\Kerberos\domain_realm' /v $identityDomainName /d $storageAccountName.file.core.windows.net
+
+reg.exe add 'HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Lsa\Kerberos\Parameters' /v CloudKerberosTicketRetrievalEnabled /t REG_DWORD /d 1
