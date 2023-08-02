bcc242b7f0912e21d8b57513599021a34bcf2a86
danycontre <dcontreras@microsoft.com>
2023-08-02 10:26:54 -0500

    update

--- workload/scripts/cleanUpRgDeployments.ps1
+++ workload/scripts/cleanUpRgDeployments.ps1
@@ -11,15 +11,18 @@ param(
 	[string]$resourceGroupName
 )
 
-$ErrorActionPreference = 'Stop'
-
 # Get powershell modules
 Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
 Install-Module -Name PowershellGet -MinimumVersion 2.2.4.1 -Force
+Install-Module 'PSDscResources' -Force
 Install-Module -Name Az.Accounts -Force
-Install-Module -Name Az.Storage -Force
-Install-Module -Name Az.Network -Force
 Install-Module -Name Az.Resources -Force
+Import-Module -Name activedirectory -Force
+
+
+Write-Log "Connecting to managed identity account"
+# Add-AzAccount -Environment $AzureCloudEnvironment -identity
+Connect-AzAccount -Identity #-AccountId $ClientId
 
 # Select subscription
 Write-Output "Selecting subscription Subscription $subscriptionId."
