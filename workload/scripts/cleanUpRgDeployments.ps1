0f9e9b2a2d67e6e34736ead88d555d4cd4abca13
danycontre <dcontreras@microsoft.com>
2023-08-02 10:55:52 -0500

    updates

--- workload/scripts/cleanUpRgDeployments.ps1
+++ workload/scripts/cleanUpRgDeployments.ps1
@@ -25,7 +25,7 @@ Select-AzSubscription -subscriptionid $subscriptionId
 
 # Get resource group succeeded deployments
 Write-Output "Getting $resourceGroupName succeeded deployments"
-$resourceGroupDeployments = Get-AzResourceGroupDeployment -ResourceGroupName rg-avd-m002-dev-usw3-pool-compute | Where-Object ProvisioningState -EQ 'Succeeded'
+$resourceGroupDeployments = Get-AzResourceGroupDeployment -ResourceGroupName $resourceGroupName | Where-Object ProvisioningState -EQ 'Succeeded'
 
 # Delete resource group deployments
 Write-Output "Deleting succeded deployments on $resourceGroupName"
