f91912e9719e3e53e8523b9fc2beae56a4819db0
Dany Contreras <78437433+danycontre@users.noreply.github.com>
2023-10-10 17:10:14 -0500

    updates

--- workload/scripts/Set-AvdAgents.ps1
+++ workload/scripts/Set-AvdAgents.ps1
@@ -5,18 +5,24 @@ Param(
         $HostPoolRegistrationToken
 )
 
-##############################################################
-#  Install the AVD Agent
-##############################################################
-# Disabling this method for installing the AVD agent until AAD Join can completed successfully
-$BootInstaller = 'AVD-Bootloader.msi'
-Get-WebFile -FileName $BootInstaller -URL 'https://query.prod.cms.rt.microsoft.com/cms/api/am/binary/RWrxrH'
-Start-Process -FilePath 'msiexec.exe' -ArgumentList "/i $BootInstaller /quiet /qn /norestart /passive" -Wait -Passthru
-Write-Log -Message 'Installed AVD Bootloader' -Type 'INFO'
+## Install AVD RD infra agent
+# Download agent
+$download = Invoke-WebRequest -Uri "https://query.prod.cms.rt.microsoft.com/cms/api/am/binary/RWrmXv" -UseBasicParsing
+$fileName = ($download.Headers.'Content-Disposition').Split('=')[1].Replace('"','')
+$output = [System.IO.FileStream]::new("$pwd\$fileName", [System.IO.FileMode]::Create)
+$output.write($download.Content, 0, $download.RawContentLength)
+$output.close()
+# Install agent
+msiexec /i $fileName /quiet REGISTRATIONTOKEN=$HostPoolRegistrationToken
 Start-Sleep -Seconds 5
 
-$AgentInstaller = 'AVD-Agent.msi'
-Get-WebFile -FileName $AgentInstaller -URL 'https://query.prod.cms.rt.microsoft.com/cms/api/am/binary/RWrmXv'
-Start-Process -FilePath 'msiexec.exe' -ArgumentList "/i $AgentInstaller /quiet /qn /norestart /passive REGISTRATIONTOKEN=$HostPoolRegistrationToken" -Wait -PassThru
-Write-Log -Message 'Installed AVD Agent' -Type 'INFO'
+## Install AVD RD infra agent
+# Download agent
+$download = Invoke-WebRequest -Uri "https://query.prod.cms.rt.microsoft.com/cms/api/am/binary/RWrxrH" -UseBasicParsing
+$fileName = ($download.Headers.'Content-Disposition').Split('=')[1].Replace('"','')
+$output = [System.IO.FileStream]::new("$pwd\$fileName", [System.IO.FileMode]::Create)
+$output.write($download.Content, 0, $download.RawContentLength)
+$output.close()
+# Install agent
+msiexec /i $fileName /quiet
 Start-Sleep -Seconds 5
