f6fbdc8bbd02320dcdfdd6609978d3a01be341a3
Dany Contreras <78437433+danycontre@users.noreply.github.com>
2023-11-01 19:27:48 -0500

    updates

--- workload/scripts/DSCStorageScripts/script-domainjoinstorage.ps1
+++ workload/scripts/DSCStorageScripts/script-domainjoinstorage.ps1
@@ -162,25 +162,18 @@ Catch {
 	Write-Log -Err $_.Exception.Message
 	Throw $_
 }
-
-Try {
-	if ($SecurityPrincipalName -eq 'none' -or $IdentityServiceProvider -eq 'AAD') {
-		Write-Log "AD group not provided or using Microsoft Entra ID joined session hosts, ACLs for AD group not set"
-	}
-	else {
 	Write-Log "setting up NTFS permission for FSLogix"
 	icacls ${DriveLetter}: /inheritance:r
 	icacls ${DriveLetter}: /remove "BUILTIN\Administrators"
 	icacls ${DriveLetter}: /grant "Creator Owner:(OI)(CI)(IO)(M)"
-	icacls ${DriveLetter}: /remove "Authenticated Users"
 	icacls ${DriveLetter}: /remove "BUILTIN\Users"
 	Write-Log "ACLs set"
-	}
 	#AVD group permissions
 	if ($SecurityPrincipalName -eq 'none' -or $IdentityServiceProvider -eq 'AAD') {
 		Write-Log "AD group not provided or using Microsoft Entra ID joined session hosts, ACLs for AD group not set"
 	}
 	else {
+		icacls ${DriveLetter}: /remove "Authenticated Users"
 		$Group = $DomainName + '\' + $SecurityPrincipalName
 		icacls ${DriveLetter}: /grant "${Group}:(M)"
 		Write-Log "AD group $Group ACLs set"
