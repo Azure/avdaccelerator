8747bec27a5799789490129fe21f9461e2515d0a
Jen Sheerin <jensheerin@microsoft.com>
2024-04-30 23:58:14 -0400

    avm updates

---
+++ workload/terraform/greenfield/ADDSscenario/outputs.tf
@@ -0,0 +1,20 @@
+output "resource" {
+  description = "This output is the full output for the resource to allow flexibility to reference all possible values for the resource. Example usage: module.<modulename>.resource.id"
+  value       = azurerm_resource_group.this
+}
+
+output "azurerm_virtual_desktop_host_pool" {
+  description = "Name of the Azure Virtual Desktop host pool"
+  value       = module.avm_res_desktopvirtualization_hostpool.resource
+
+}
+
+output "azurerm_virtual_desktop_application_group" {
+  description = "Name of the Azure Virtual Desktop DAG"
+  value       = module.avm_res_desktopvirtualization_applicationgroup.resource
+}
+
+output "azurerm_virtual_desktop_workspace" {
+  description = "Name of the Azure Virtual Desktop workspace"
+  value       = module.avm_res_desktopvirtualization_workspace.resource
+}
