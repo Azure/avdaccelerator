944ad3cd883dad575098d13d8320f14829e26274
Joel Peterson <54716797+petersonjdNIH@users.noreply.github.com>
2024-05-02 19:24:50 -0700

    Updated Destroy

--- .github/workflows/scripts/Delete_RGs.sh
+++ .github/workflows/scripts/Delete_RGs.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 # List of resource groups to delete
-resource_groups=("rg-avd-avdn-test-use2-network" "rg-avd-avdn-test-use2-service-objects" "rg-avd-avdn-test-use2-pool-compute" "rg-avd-avdn-test-use2-storage" "rg-avd-test-use2-monitoring")
+resource_groups=("avd-nih-arpah-test-use2-monitoring" "avd-nih-arpah-test-use2-network" "avd-nih-arpah-test-use2-pool-compute" "avd-nih-arpah-test-use2-service-objects" "rg-avd-test-use2-monitoring")
 
 # Loop through each resource group and delete
 for rg in "${resource_groups[@]}"; do
@@ -9,4 +9,4 @@ for rg in "${resource_groups[@]}"; do
     az group delete --name $rg --yes --no-wait
 done
 
-echo "Resource groups deletion requested. Check Azure portal for status."
+echo "Resource groups deletion requested. Check Azure for status."
