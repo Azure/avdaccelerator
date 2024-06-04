45cc181bf84161a2545cbff683c6ff383e66e444
Jen Sheerin <jensheerin@microsoft.com>
2024-06-04 12:25:09 -0400

    avm alignment

--- workload/terraform/greenfield/AADscenario/rbac.tf
+++ workload/terraform/greenfield/AADscenario/rbac.tf
@@ -8,7 +8,7 @@ data "azuread_group" "adds_group" {
 }
 
 resource "azurerm_role_assignment" "role" {
-  scope              = module.avm-res-desktopvirtualization-applicationgroup.azurerm_virtual_desktop_application_group_id
+  scope              = module.avm-ptn-avd-lza-managementplane.virtual_desktop_application_group_name.id
   role_definition_id = data.azurerm_role_definition.role.id
   principal_id       = data.azuread_group.adds_group.id
 }
