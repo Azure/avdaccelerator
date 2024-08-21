36e2b962f18b50504c1950b2d0d2758f7738a1b6
John Ireland <41406288+jwaltireland@users.noreply.github.com>
2024-08-21 14:46:19 -0400

    updates

--- .github/workflows/scripts/Delete_RGs.sh
+++ .github/workflows/scripts/Delete_RGs.sh
@@ -1,7 +1,10 @@
 #! /bin/bash
 
+echo "Environment: $1"
+
 # List of resource groups to delete
-resource_groups=("avd-nih-arpah-test-use2-monitoring" "avd-nih-arpah-test-use2-network" "avd-nih-arpah-test-use2-pool-compute" "avd-nih-arpah-test-use2-service-objects" "rg-avd-test-use2-monitoring")
+#resource_groups=("avd-nih-arpah-test-use2-monitoring" "avd-nih-arpah-test-use2-network" "avd-nih-arpah-test-use2-pool-compute" "avd-nih-arpah-test-use2-service-objects" "rg-avd-test-use2-monitoring")
+resource_groups=("avd-nih-arpah-$1-use2-monitoring" "avd-nih-arpah-$1-use2-network" "avd-nih-arpah-$1-use2-pool-compute" "avd-nih-arpah-$1-use2-service-objects" "rg-avd-$1-use2-monitoring")
 
 # Loop through each resource group and delete
 for rg in "${resource_groups[@]}"; do
