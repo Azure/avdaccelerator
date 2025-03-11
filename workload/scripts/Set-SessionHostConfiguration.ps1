d353d5d240fac42f62d12937b0215d4761f0a71f
Shawn Meyer <shmeyer@microsoft.com>
2025-03-11 11:12:43 -0400

    fixed minor bug in set sessionhost script

--- workload/scripts/Set-SessionHostConfiguration.ps1
+++ workload/scripts/Set-SessionHostConfiguration.ps1
@@ -312,7 +312,7 @@ try {
                 Reg LOAD HKLM\DefaultUser "$env:SystemDrive\Users\Default User\NtUser.dat"
                 Set-RegistryValue -Key 'HKLM:\DefaultUser\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer' -Name NoRecycleFiles -Type DWord -Value 1
                 Write-Log -Message "Unloading default user hive."
-                $null = cmd /c REG UNLOAD "HKLM\Default" '2>&1'
+                $null = cmd /c REG UNLOAD "HKLM\DefaultUser" '2>&1'
         }
 
         ##############################################################
