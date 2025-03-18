0168482ce761f704365ffa26b2cf657693005411
John Ireland <41406288+jwaltireland@users.noreply.github.com>
2025-03-18 13:46:31 -0400

    clean up

---
+++ .github/workflows/scripts/DeploymentStack-DeveloperSessionHost.ps1
@@ -0,0 +1,42 @@
+﻿param (
+    [string]$DeploymentStackName,
+    [string]$Location,
+    [string]$TemplateFile,
+    [string]$ParametersFile,
+    [string]$avdSessionHostCustomNamePrefix,
+    [string]$deploymentEnvironment,
+    [string]$avdWorkloadSubsId,
+    [string]$imageGallerySubscriptionId,
+    [string]$existingVnetAvdSubnetResourceId,
+    [string]$existingVnetPrivateEndpointSubnetResourceId,
+    [string]$identityDomainName, 
+    [string]$avdOuPath,
+    [string]$securityPrincipalId,
+    [string]$update_existing_stack
+)
+
+if ($update_existing_stack -eq 'true') {
+    Write-Host "Updating existing stack"
+    Set-AzSubscriptionDeploymentStack -Name $DeploymentStackName -Location $Location -TemplateFile $TemplateFile -TemplateParameterFile $ParametersFile -P -ActionOnUnmanage "detachAll" -DenySettingsMode "none" `
+        -avdSessionHostCustomNamePrefix $avdSessionHostCustomNamePrefix -deploymentEnvironment $deploymentEnvironment -avdWorkloadSubsId $avdWorkloadSubsId -imageGallerySubscriptionId $imageGallerySubscriptionId `
+        -existingVnetAvdSubnetResourceId $existingVnetAvdSubnetResourceId -existingVnetPrivateEndpointSubnetResourceId $existingVnetPrivateEndpointSubnetResourceId `
+        -identityDomainName $identityDomainName -avdOuPath $avdOuPath -securityPrincipalId $securityPrincipalId
+    return
+} else {
+    Write-Host "Creating new stack"
+    New-AzSubscriptionDeploymentStack -Name $DeploymentStackName -Location $Location -TemplateFile $TemplateFile -TemplateParameterFile $ParametersFile -P -ActionOnUnmanage "detachAll" -DenySettingsMode "none" `
+    -avdSessionHostCustomNamePrefix $avdSessionHostCustomNamePrefix -deploymentEnvironment $deploymentEnvironment -avdWorkloadSubsId $avdWorkloadSubsId -imageGallerySubscriptionId $imageGallerySubscriptionId `
+    -existingVnetAvdSubnetResourceId $existingVnetAvdSubnetResourceId -existingVnetPrivateEndpointSubnetResourceId $existingVnetPrivateEndpointSubnetResourceId `
+    -identityDomainName $identityDomainName -avdOuPath $avdOuPath -securityPrincipalId $securityPrincipalId
+    return
+}
+
+# New-AzSubscriptionDeploymentStack -Name $DeploymentStackName -Location $Location -TemplateFile $TemplateFile -TemplateParameterFile $ParametersFile -P -ActionOnUnmanage "detachAll" -DenySettingsMode "none" `
+#     -avdSessionHostCustomNamePrefix $avdSessionHostCustomNamePrefix -deploymentEnvironment $deploymentEnvironment -avdWorkloadSubsId $avdWorkloadSubsId -imageGallerySubscriptionId $imageGallerySubscriptionId `
+#     -avdEnterpriseAppObjectId $avdEnterpriseAppObjectId -existingVnetAvdSubnetResourceId $existingVnetAvdSubnetResourceId -existingVnetPrivateEndpointSubnetResourceId $existingVnetPrivateEndpointSubnetResourceId `
+#     -identityDomainName $identityDomainName -avdOuPath $avdOuPath -securityPrincipalId $securityPrincipalId
+
+# Set-AzSubscriptionDeploymentStack -Name $DeploymentStackName -Location $Location -TemplateFile $TemplateFile -ActionOnUnmanage "detachAll" -DenySettingsMode "none" `
+#     -avdSessionHostCustomNamePrefix $avdSessionHostCustomNamePrefix -deploymentEnvironment $deploymentEnvironment -avdWorkloadSubsId $avdWorkloadSubsId -imageGallerySubscriptionId $imageGallerySubscriptionId `
+#     -avdEnterpriseAppObjectId $avdEnterpriseAppObjectId -existingVnetAvdSubnetResourceId $existingVnetAvdSubnetResourceId -existingVnetPrivateEndpointSubnetResourceId $existingVnetPrivateEndpointSubnetResourceId `
+#     -identityDomainName $identityDomainName -avdOuPath $avdOuPath -securityPrincipalId $securityPrincipalId
