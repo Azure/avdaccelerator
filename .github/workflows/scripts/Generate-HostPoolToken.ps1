0b4148e15592cd1b88d95c7a47b617c81a29bb51
John Ireland <41406288+jwaltireland@users.noreply.github.com>
2025-03-21 10:21:06 -0400

    added new yml and ps1 for generating host pool token

---
+++ .github/workflows/scripts/Generate-HostPoolToken.ps1
@@ -0,0 +1,14 @@
+﻿<#
+    Updates the AVD application display name.
+#>
+param (
+    [string]$HostPoolResourceGroupName,
+    [string]$HostPoolName,
+    [string]$KeyVaultName
+)
+
+Write-PSFMessage -Level Host -Message "Generating new token for the host pool {0} in Resource Group {1}" -StringValues $HostPoolName, $HostPoolResourceGroupName
+$hostPoolToken = New-AzWvdRegistrationInfo -ResourceGroupName $HostPoolResourceGroupName -HostPoolName $HostPoolName -ExpirationTime (Get-Date).AddHours(2) -ErrorAction Stop
+
+# now update key vault
+az keyvault secret set --name hostPoolRegistrationToken --vault-name $KeyVaultName --value $hostPoolToken
