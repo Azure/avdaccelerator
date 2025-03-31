976e9ffd22780ea399c76724b4d2aac954368431
John Ireland <41406288+jwaltireland@users.noreply.github.com>
2025-03-31 15:34:56 -0400

    updates

--- .github/workflows/scripts/DeploymentStack-DeveloperHostPool.ps1
+++ .github/workflows/scripts/DeploymentStack-DeveloperHostPool.ps1
@@ -8,19 +8,20 @@
     [string]$avdEnterpriseAppObjectId,
     [string]$existingVnetPrivateEndpointSubnetResourceId,
     [string]$securityPrincipalId,
-    [string]$update_existing_stack
+    [string]$update_existing_stack,
+    [string]$avdHostPoolType
 )
 
 if ($update_existing_stack -eq 'true') {
     Write-Host "Updating existing stack"
     Set-AzSubscriptionDeploymentStack -Name $DeploymentStackName -Location $Location -TemplateFile $TemplateFile -TemplateParameterFile $ParametersFile -ActionOnUnmanage "detachAll" -DenySettingsMode "none" `
         -deploymentEnvironment $deploymentEnvironment -avdWorkloadSubsId $avdWorkloadSubsId -avdEnterpriseAppObjectId $avdEnterpriseAppObjectId -existingVnetPrivateEndpointSubnetResourceId $existingVnetPrivateEndpointSubnetResourceId `
-        -securityPrincipalId $securityPrincipalId
+        -securityPrincipalId $securityPrincipalId -avdHostPoolType $avdHostPoolType
     return
 } else {
     Write-Host "Creating new stack"
     New-AzSubscriptionDeploymentStack -Name $DeploymentStackName -Location $Location -TemplateFile $TemplateFile -TemplateParameterFile $ParametersFile -ActionOnUnmanage "detachAll" -DenySettingsMode "none" `
         -deploymentEnvironment $deploymentEnvironment -avdWorkloadSubsId $avdWorkloadSubsId -avdEnterpriseAppObjectId $avdEnterpriseAppObjectId -existingVnetPrivateEndpointSubnetResourceId $existingVnetPrivateEndpointSubnetResourceId `
-        -securityPrincipalId $securityPrincipalId
+        -securityPrincipalId $securityPrincipalId -avdHostPoolType $avdHostPoolType
     return
 }
